6c3bb34513dd6327b64a73723a9dde8d20f18d0e
ZoutigeWolf <guuskamphuis@gmail.com>
2023-12-03 21:32:40 +0100

    Initial Commit

---
+++ VisconSupport/VisconSupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisconSupportApp.swift
+//  VisconSupport
+//
+//  Created by ZoutigeWolf on 03/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct VisconSupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
